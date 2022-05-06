706a474d17cd892cf2d0d90533acc72cecb9758f
cpak125 <chris.pak125@gmail.com>
2022-05-06 14:31:53 -0400

    Add markup

---
+++ js/main.js
@@ -0,0 +1,28 @@
+/*
+State variables:
+1. board (tracking the state of the board)
+2. turn (who's turn is it?)
+3. winner (is there a wiiner, and if so, who?)
+*/
+
+
+
+/*----- constants -----*/
+
+
+
+/*----- app's state (variables) -----*/
+
+
+
+/*----- cached element references -----*/
+
+
+
+/*----- event listeners -----*/
+
+
+
+/*----- functions -----*/
+
+
